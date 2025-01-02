d7507afa85eeeb202b44343f7eeb32f80b903cb2
Valter Andrei <valterandrey@gmail.com>
2025-01-02 18:24:10 +0000

    Set JWT expiration time to 1 day

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,7 +38,7 @@ Devise.setup do |config|
     ]
 
     # jwt.expiration_time = 30.minutes.to_i
-    # jwt.expiration_time = 1.day.to_i
+    jwt.expiration_time = 1.day.to_i
   end
 
   # Configure the class responsible to send e-mails.
